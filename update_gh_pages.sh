7e5d3891fad57dc914738fafde71bdc65db939c9
sonota88 <yosiot8753@gmail.com>
2016-08-04 23:29:55 +0900

    refactor: インデントの修正のみ

--- update_gh_pages.sh
+++ update_gh_pages.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 update() {
-local version="$1"; shift
+  local version="$1"; shift
 
-timestamp=$(date "+%Y%m%d_%H%M%S")
-temp_dir="/tmp/apache-spark-docs-ja/${timestamp}"
+  timestamp=$(date "+%Y%m%d_%H%M%S")
+  temp_dir="/tmp/apache-spark-docs-ja/${timestamp}"
 
-mkdir -p $temp_dir
+  mkdir -p $temp_dir
 
-git co "v${version}"
-./build.sh
+  git co "v${version}"
+  ./build.sh
 
-cp -r build/_site "${temp_dir}/${version}"
+  cp -r build/_site "${temp_dir}/${version}"
 
-git co gh-pages
-rm -rf "${version}"
-cp -r "${temp_dir}/${version}" .
+  git co gh-pages
+  rm -rf "${version}"
+  cp -r "${temp_dir}/${version}" .
 }
 
 update "$1"
